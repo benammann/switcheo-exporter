9df5b0d99f30489a62496e836582a1bb98eb3aa1
Ben Ammann <24310981+benammann@users.noreply.github.com>
2018-11-28 13:57:45 +0100

    REF: #7 reload address when toggling Network

--- src/actions/switcheo/toggleNetwork.js
+++ src/actions/switcheo/toggleNetwork.js
@@ -3,15 +3,23 @@ import { SWITCHEO_TOGGLE_NETWORK } from "./actions";
 import { fetchContracts } from './fetchContracts'
 import { fetchTokens } from './fetchTokens'
 
+import { setAddress } from "../address/setAddress";
+
 /**
  * toggles the current network between test-api and prod-api
  * it also fetches tokens and contracts from the given network
  * @returns {function(*): *}
  */
-export const toggleNetwork = () => dispatch => {
+export const toggleNetwork = () => (dispatch, getState) => {
     dispatch(fetchContracts());
     dispatch(fetchTokens());
-    return dispatch({
+    dispatch({
         type: SWITCHEO_TOGGLE_NETWORK,
-    })
+    });
+    if(getState().address.address) {
+        /**
+         * In case an address is set, its orders need to reload and rerender
+         */
+        dispatch(setAddress(getState().address.address, true))
+    }
 };
