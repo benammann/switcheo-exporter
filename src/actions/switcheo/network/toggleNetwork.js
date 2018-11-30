6249942ea331604d4655c9eed7d8652994041c5e
Ben Ammann <24310981+benammann@users.noreply.github.com>
2018-11-30 14:09:50 +0100

    Refactored Error Dialog

--- src/actions/switcheo/network/toggleNetwork.js
+++ src/actions/switcheo/network/toggleNetwork.js
@@ -4,8 +4,6 @@ import { fetchContracts } from '../contracts/fetchContracts'
 import { fetchTokens } from '../tokens/fetchTokens'
 
 import { setAddress } from "../../address/address/setAddress";
-import { raiseError } from "../../layout/errors/raiseError";
-import { mainNet} from "../../../reducers/switcheo/networkReducer";
 
 /**
  * toggles the current network between test-api and prod-api
@@ -23,9 +21,5 @@ export const toggleNetwork = () => (dispatch, getState) => {
          * In case an address is set, its orders need to reload and rerender
          */
         dispatch(setAddress(getState().address.address.address, true));
-        /**
-         * TODO: implement raiseNotification
-         */
-        dispatch(raiseError(`switched to ${getState().switcheo.network === mainNet ? "MainNet" : "TestNet"}`));
     }
 };
--- src/components/ErrorProvider/index.js
+++ src/components/ErrorProvider/index.js
@@ -9,29 +9,23 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Typography from '@material-ui/core/Typography'
 
 class ErrorProvider extends Component {
 
     render() {
         return (
             <div>
-            <Dialog
-                fullScreen={false}
-                open={this.props.layout.error.show}
-                onClose={this.props.hideError}
-                aria-labelledby="responsive-dialog-title"
-                color={"primary"}
-            >
-                <DialogContent>
-                    <DialogContentText>{this.props.layout.error.message}</DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={this.props.hideError} color="primary" autoFocus>
-                        OK
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                <Dialog open={this.props.layout.error.show} onClose={this.props.hideError} aria-labelledby={"error-dialog"}>
+                    <DialogTitle id={"error-dialog"}>Error</DialogTitle>
+                    <DialogContent>
+                        <Typography component={"b"}>{this.props.layout.error.message}</Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.props.hideError} color={"primary"} variant={"outlined"}>Close</Button>
+                    </DialogActions>
+                </Dialog>
                 {this.props.children}
             </div>
         )
